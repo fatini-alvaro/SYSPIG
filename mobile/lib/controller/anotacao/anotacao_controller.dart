783f243342bb6606002a4aa33a27c877914d0645
fatini-alvaro <alvarofatini@gmail.com>
2024-02-13 15:59:51 -0300

    fix anotacaoControler

--- mobile/lib/controller/anotacao/anotacao_controller.dart
+++ mobile/lib/controller/anotacao/anotacao_controller.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/model/anotacao_model.dart';
-import 'package:mobile/repositories/anotacao/animal_repository.dart';
+import 'package:mobile/repositories/anotacao/anotacao_repository.dart';
 
 class AnotacaoController {
   final AnotacaoRepository _anotacaoRepository;
--- mobile/lib/repositories/anotacao/anotacao_repository_imp.dart
+++ mobile/lib/repositories/anotacao/anotacao_repository_imp.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:mobile/model/anotacao_model.dart';
-import 'package:mobile/repositories/anotacao/animal_repository.dart';
+import 'package:mobile/repositories/anotacao/anotacao_repository.dart';
 
 class AnotacaoRepositoryImp implements AnotacaoRepository {   
   @override
